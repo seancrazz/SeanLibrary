74da565dc982bf90835b4c94b252becfac77df15
seans <seans@hexagontech.co.il>
2018-01-09 12:30:29 +0200

    Updated input validation. Project is ready.

--- TypeScriptHTMLApp9/TypeScriptHTMLApp9/app.js
+++ TypeScriptHTMLApp9/TypeScriptHTMLApp9/app.js
@@ -1,15 +1,91 @@
-////////////////// MAIN SECTION /////////////////////////////////////////////
-var shapeLength = Number(prompt("What is the Limp length"));
-triangleNumbers(shapeLength);
-///////////////////////////// General ///////////////////////////////////////////
+//////////////////// MAIN SECTION /////////////////////////////////////////////
+///// Global Variables 
+var shape;
+var shapeSideLenght;
+var shapeWidthLenght;
+var shapeVersion;
+//////////// FUNCTIONS CALL //////////////
+if (mainFunctioninputs()) {
+    drawThis();
+}
+/////////////// FUNCTIONS SECTION //////////////
+function mainFunctioninputs() {
+    shape = Number(prompt("Please Select Your Shape: \n 1. Square \n 2. Triangele \n 3. Rectangle"));
+    if (shape != 1 && shape != 2 && shape != 3) {
+        alert("You Didn't Chose The Correct Option - Goodbye");
+        return false;
+    }
+    shapeSideLenght = Number(prompt("Please Enter Shape Side Lenght "));
+    if (parseInt(shapeSideLenght.toString()) != shapeSideLenght) {
+        alert("You Didn't Insert A Valid Parameter - Goodbye! ");
+        return false;
+    }
+    if (shape == 3) {
+        shapeWidthLenght = Number(prompt("Please Enter Shape Width Lenght "));
+        if (parseInt(shapeWidthLenght.toString()) != shapeWidthLenght) {
+            alert("YYou Didn't Insert A Valid Parameter - Goodbye!");
+            return false;
+        }
+        1;
+    }
+    shapeVersion = Number(prompt("Please select the shape type you would like to recieve: \n 1. Shape Made Of Asterisks \n 2. Shape Made Of Ascending Numbers (Made of the Inseted Number) \n 3.Shape Made Of Descending Numbers (Made of the Inseted Number) \n 4.Shape Made Of Asterisks Frame."));
+    if (shapeVersion != 1 && shapeVersion != 2 && shapeVersion != 3 && shapeVersion != 4) {
+        alert("You Didn't Chose The Correct Option - Goodbye");
+        return false;
+    }
+    return true;
+}
+function drawThis() {
+    ////SQUARE
+    if (shape == 1 && shapeVersion == 1) {
+        squerPrint2(shapeSideLenght);
+    }
+    if (shape == 1 && shapeVersion == 2) {
+        squerPrint(shapeSideLenght);
+    }
+    if (shape == 1 && shapeVersion == 3) {
+        squer1Print(shapeSideLenght);
+    }
+    if (shape == 1 && shapeVersion == 4) {
+        squerPrint3(shapeSideLenght);
+    }
+    /// TRIANGLE 
+    if (shape == 2 && shapeVersion == 1) {
+        triangle1(shapeSideLenght);
+    }
+    if (shape == 2 && shapeVersion == 2) {
+        triangleNumbers(shapeSideLenght);
+    }
+    if (shape == 2 && shapeVersion == 3) {
+        triangleNumbersReverseOrder(shapeSideLenght);
+    }
+    if (shape == 2 && shapeVersion == 4) {
+        triangle1Contour(shapeSideLenght);
+    }
+    /// RETRIANGLE 
+    if (shape == 3 && shapeVersion == 1) {
+        asteriskRectangle(shapeSideLenght, shapeWidthLenght);
+    }
+    if (shape == 3 && shapeVersion == 2) {
+        rectangleNumber(shapeSideLenght, shapeWidthLenght);
+    }
+    if (shape == 3 && shapeVersion == 3) {
+        rectangleNumberRevers(shapeSideLenght, shapeWidthLenght);
+    }
+    if (shape == 3 && shapeVersion == 4) {
+        asteriskRectangleFrame(shapeSideLenght, shapeWidthLenght);
+    }
+}
+///////////////////////////// FONT FUNCTIONS ///////////////////////////////////////////
 function writeEmptyChar() {
     document.write("<font color=white>&nbsp&nbsp*&nbsp&nbsp</font>");
 }
 function writeFullChar() {
     document.write("&nbsp&nbsp*&nbsp&nbsp");
 }
-////////////////////////////////////////////////////////////////////////////////////// Squre Print //////////////////////////////////////////////////////////////////////
-/////// Square Ascending Numbers
+////////////////////////////////////////////////////////////////////////////////////// DRAWER FUNCTIONS //////////////////////////////////////////////////////////////////////
+//////////////SQUARE////////////////////////////////////////////////SQUARE///////////////////////////////SQUARE/////////////////////////////////////////SQUARE/////////////////////////////////////////////
+///////// Square Ascending Numbers
 function squerPrint(side) {
     for (var outerIndex = 1; outerIndex <= side; outerIndex++) {
         for (var innerIndex = 1; innerIndex <= side; innerIndex++) {
@@ -51,17 +127,13 @@ function squerPrint3(rrSideLenght) {
         document.write("<br/>");
     }
 }
-/////////////////////////////////////////// Triangle //////////////////////////////////////////////////////////////
+/////////////////////////////Triangle////////////////////////////////////////Triangle//////////////////////////////////// Triangle /////////////////////////////////////////////Triangle/////////////////
 //////// Triangle Asterisk Full 
 function triangle1(sideLength) {
     var height = sideLength;
     var width = 1;
     for (var rownum = 1; rownum <= height; rownum++) {
         for (var column = 1; column <= width; column++) {
-            //if (rownum < rrSideLenght && rownum > 1 && colnum < rrSideLenght && colnum > 1) {
-            //    document.write(`&nbsp`);
-            //}
-            //else {
             writeFullChar();
         }
         width++;
@@ -109,6 +181,44 @@ function triangleNumbersReverseOrder(sideLength) {
         }
         width++;
         document.write("<br/> ");
+    }
+}
+/////////////////////////////////Rectangle///////////////////////// Rectangle //////////////////////////////////Rectangle///////////////////////////////////Rectangle///////////////////////////////////////////
+function asteriskRectangleFrame(height, width) {
+    for (var rownum = 0; rownum < height; rownum++) {
+        for (var colnum = 0; colnum < width; colnum++) {
+            if (colnum == 0 || colnum == width - 1 || rownum == 0 || rownum == height - 1) {
+                writeFullChar();
+            }
+            else {
+                writeEmptyChar();
+            }
+        }
+        document.write("<br/>");
+    }
+}
+function asteriskRectangle(row, column) {
+    for (var outerIndex = 1; outerIndex <= row; outerIndex++) {
+        for (var innerIndex = 1; innerIndex <= column; innerIndex++) {
+            writeFullChar();
+        }
+        document.write("<br/>");
+    }
+}
+function rectangleNumber(row, column) {
+    for (var outerIndex = 1; outerIndex <= row; outerIndex++) {
+        for (var innerIndex = 1; innerIndex <= column; innerIndex++) {
+            document.write("" + innerIndex);
+        }
+        document.write("<br/>");
+    }
+}
+function rectangleNumberRevers(row, column) {
+    for (var outerIndex = 1; outerIndex <= row; outerIndex++) {
+        for (var innerIndex = column; innerIndex >= 1; innerIndex--) {
+            document.write(innerIndex + " ");
+        }
+        document.write("<br/>");
     }
 }
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp9/TypeScriptHTMLApp9/app.ts
+++ TypeScriptHTMLApp9/TypeScriptHTMLApp9/app.ts
@@ -1,29 +1,124 @@
 ﻿
 
+//////////////////// MAIN SECTION /////////////////////////////////////////////
+
+///// Global Variables 
+var shape: number;
+var shapeSideLenght: number;
+var shapeWidthLenght: number;
+var shapeVersion: number;
+
+//////////// FUNCTIONS CALL //////////////
+
+if (mainFunctioninputs()) {
+    drawThis();    
+}
+
+/////////////// FUNCTIONS SECTION //////////////
+function mainFunctioninputs(): boolean {
+
+    shape = Number(prompt("Please Select Your Shape: \n 1. Square \n 2. Triangele \n 3. Rectangle"));
+
+    if (shape != 1 && shape != 2 && shape != 3) {
+        alert(`You Didn't Chose The Correct Option - Goodbye`);
+        return false;
+    }
+    shapeSideLenght = Number(prompt("Please Enter Shape Side Lenght "));
+    if (parseInt(shapeSideLenght.toString()) != shapeSideLenght) {
+        alert("You Didn't Insert A Valid Parameter - Goodbye! ")
+        return false;
+    }
+    if (shape == 3) {
+        shapeWidthLenght = Number(prompt("Please Enter Shape Width Lenght "));
+        if (parseInt(shapeWidthLenght.toString()) != shapeWidthLenght) {
+            alert("YYou Didn't Insert A Valid Parameter - Goodbye!")
+            return false;
+        }1
+    }
+    shapeVersion = Number(prompt("Please select the shape type you would like to recieve: \n 1. Shape Made Of Asterisks \n 2. Shape Made Of Ascending Numbers (Made of the Inseted Number) \n 3.Shape Made Of Descending Numbers (Made of the Inseted Number) \n 4.Shape Made Of Asterisks Frame."));
+    if (shapeVersion != 1 && shapeVersion != 2 && shapeVersion != 3 && shapeVersion != 4) {
+        alert(`You Didn't Chose The Correct Option - Goodbye`);
+        return false;
+    }
+    return true;
+}
 
 
+function drawThis(): void {
+
+    ////SQUARE
+    if (shape == 1 && shapeVersion == 1) {
+
+        squerPrint2(shapeSideLenght);
+
+    }
+    if (shape == 1 && shapeVersion == 2) {
+        squerPrint(shapeSideLenght);
+    }
+
+    if (shape == 1 && shapeVersion == 3) {
+        squer1Print(shapeSideLenght);
+    }
+    if (shape == 1 && shapeVersion == 4) {
+        squerPrint3(shapeSideLenght);
+    }
+
+    /// TRIANGLE 
+
+    if (shape == 2 && shapeVersion == 1) {
+        triangle1(shapeSideLenght);
+    }
+    if (shape == 2 && shapeVersion == 2) {
+        triangleNumbers(shapeSideLenght);
+    }
+
+    if (shape == 2 && shapeVersion == 3) {
+        triangleNumbersReverseOrder(shapeSideLenght);
+    }
+
+    if (shape == 2 && shapeVersion == 4) {
+        triangle1Contour(shapeSideLenght);
+    }
+
+    /// RETRIANGLE 
 
 
-////////////////// MAIN SECTION /////////////////////////////////////////////
+    if (shape == 3 && shapeVersion == 1) {
+        asteriskRectangle(shapeSideLenght, shapeWidthLenght);
+    }
+    if (shape == 3 && shapeVersion == 2) {
+        rectangleNumber(shapeSideLenght, shapeWidthLenght);
+    }
 
-var shapeLength: number = Number(prompt("What is the Limp length"));
+    if (shape == 3 && shapeVersion == 3) {
 
-triangleNumbers(shapeLength);
+        rectangleNumberRevers(shapeSideLenght, shapeWidthLenght);
+    }
 
-///////////////////////////// General ///////////////////////////////////////////
+    if (shape == 3 && shapeVersion == 4) {
+
+        asteriskRectangleFrame(shapeSideLenght, shapeWidthLenght);
+    }
+}
+
+///////////////////////////// FONT FUNCTIONS ///////////////////////////////////////////
 
 function writeEmptyChar(): void {
     document.write(`<font color=white>&nbsp&nbsp*&nbsp&nbsp</font>`);
 }
-
-
 function writeFullChar(): void {
     document.write(`&nbsp&nbsp*&nbsp&nbsp`);
 }
 
-////////////////////////////////////////////////////////////////////////////////////// Squre Print //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////// DRAWER FUNCTIONS //////////////////////////////////////////////////////////////////////
 
-/////// Square Ascending Numbers
+
+
+//////////////SQUARE////////////////////////////////////////////////SQUARE///////////////////////////////SQUARE/////////////////////////////////////////SQUARE/////////////////////////////////////////////
+
+
+
+///////// Square Ascending Numbers
 function squerPrint(side: number): void {
 
     for (let outerIndex: number = 1; outerIndex <= side; outerIndex++) {
@@ -35,7 +130,6 @@ function squerPrint(side: number): void {
         document.write("<br/>");
     }
 }
-
 
 ///// Square Descending Numbers
 
@@ -51,27 +145,16 @@ function squer1Print(side1: number): void {
     }
 
 }
-
 ////  Asterisk Square
-
 function squerPrint2(side3: number): void {
-
     for (let outerIndex: number = 1; outerIndex <= side3; outerIndex++) {
         for (let innerIndex2: number = 1; innerIndex2 <= side3; innerIndex2++) {
-
             writeFullChar();
         }
-
         document.write("<br/>");
     }
 }
-
-
-
-
 ///// Asterisk Square Frame
-
-
 function squerPrint3(rrSideLenght: number): void {
 
     for (let rownum: number = 1; rownum <= rrSideLenght; rownum++) {
@@ -86,50 +169,25 @@ function squerPrint3(rrSideLenght: number): void {
         }
         document.write(`<br/>`);
     }
-
 }
-
-
-
-
-
-/////////////////////////////////////////// Triangle //////////////////////////////////////////////////////////////
-
-
+/////////////////////////////Triangle////////////////////////////////////////Triangle//////////////////////////////////// Triangle /////////////////////////////////////////////Triangle/////////////////
 
 //////// Triangle Asterisk Full 
-
 function triangle1(sideLength: number): void {
-
     let height: number = sideLength;
     let width: number = 1;
     for (let rownum: number = 1; rownum <= height; rownum++) {
         for (let column: number = 1; column <= width; column++) {
-            //if (rownum < rrSideLenght && rownum > 1 && colnum < rrSideLenght && colnum > 1) {
-            //    document.write(`&nbsp`);
-            //}
-            //else {
             writeFullChar();
-       
         }
         width++;
         document.write(`<br/> `);
     }
-
 }
-
-
-
-
 ///// Triangle Asterisk Frame
-
 function triangle1Contour(sideLength: number): void {
-
     let height: number = sideLength;
     let width: number = 1;
-
-
-
     for (let rownum: number = 1; rownum <= height; rownum++) {
         for (let column: number = 1; column <= width; column++) {
             if (rownum == 1 || rownum == height || column == 1 || column == width) {
@@ -142,11 +200,8 @@ function triangle1Contour(sideLength: number): void {
         width++;
         document.write(`<br/> `);
     }
-
 }
-
 /////// Triangle Number Acsending
-
 function triangleNumbers(sideLength: number): void {
 
     let height: number = sideLength;
@@ -163,21 +218,70 @@ function triangleNumbers(sideLength: number): void {
 }
 
 /////Triangle Number Decsending
-
 function triangleNumbersReverseOrder(sideLength: number): void {
-
     let height: number = sideLength;
     let width: number = 1;
-
     for (let rownum: number = 1; rownum <= height; rownum++) {
         let symbolNum: number = sideLength;
         for (let column: number = 1; column <= width; column++) {
-        
             document.write(`${symbolNum}`);
             symbolNum--;
         }
         width++;
         document.write(`<br/> `);
     }
+}
 
+/////////////////////////////////Rectangle///////////////////////// Rectangle //////////////////////////////////Rectangle///////////////////////////////////Rectangle///////////////////////////////////////////
+
+function asteriskRectangleFrame(height: number, width: number): void {
+
+    for (let rownum: number = 0; rownum < height; rownum++) {
+        for (let colnum: number = 0; colnum < width; colnum++) {
+            if (colnum == 0 || colnum == width - 1 || rownum == 0 || rownum == height - 1) {
+
+                writeFullChar();
+            }
+            else {
+                writeEmptyChar();
+            }
+        }
+        document.write(`<br/>`);
+    }
+}
+
+function asteriskRectangle(row: number, column: number): void {
+
+    for (let outerIndex: number = 1; outerIndex <= row; outerIndex++) {
+        for (let innerIndex: number = 1; innerIndex <= column; innerIndex++) {
+
+            writeFullChar();
+        }
+
+        document.write("<br/>");
+    }
+}
+
+function rectangleNumber(row: number, column: number): void {
+
+    for (let outerIndex: number = 1; outerIndex <= row; outerIndex++) {///// rows
+
+        for (let innerIndex: number = 1; innerIndex <= column; innerIndex++) {///// columns
+
+            document.write(`${innerIndex}`);
+        }
+        document.write("<br/>");
+    }
+}
+
+function rectangleNumberRevers(row: number, column: number): void {
+
+    for (let outerIndex: number = 1; outerIndex <= row; outerIndex++) {///// rows
+        for (let innerIndex: number = column; innerIndex >= 1; innerIndex--) {///// columns
+
+            document.write(`${innerIndex} `);
+        }
+
+        document.write("<br/>");
+    }
 }
